dae633379aa2e4e84611e8e04ba51c9173c31a55
Alex O Skramstad <askramst@silk3.uvm.edu>
2015-10-07 20:50:52 -0400

    commit

---
+++ bin/pass.php
@@ -0,0 +1,6 @@
+<?php
+// passwords for your database 
+$dbReader="YJh5WBK5O3SR";
+$dbWriter="qnJfUmAXfZl6";
+$dbAdmin="gtenf1mJIbg4";
+?>
--- sitemap.php
+++ sitemap.php
@@ -2,7 +2,7 @@
     <title>SITE MAP </title>
     <li><a href="assignment1.0/index.php">Assignment1.0</a></li>   
     <li><a href="assignment2.0/select.php">Assignment2.0: SELECT</a></li>  
-    <li><a href="assignment3.0/select.php">Assignment3.0: JOIN</a></li>
+    <li><a href="assignment3.0/join.php">Assignment3.0: JOIN</a></li>
     <footer> Website Designed by a Dog Eating Peanut Butter</footer>
     <iframe width="560" height="315" src="https://www.youtube.com/embed/4Ri5cszSKEg" frameborder="0" allowfullscreen></iframe>
     
